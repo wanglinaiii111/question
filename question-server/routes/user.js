2b7eb4dfbfae9d96d1f951c5e8044c7a0a117f11
zhangw <786014381@qq.com>
2021-05-26 19:17:30 +0800

    修改登录接口

--- question-server/routes/user.js
+++ question-server/routes/user.js
@@ -7,14 +7,21 @@ const userDao = require('../dao/userDao');
 router.post('/login', async function (req, res, next) {
     let p = req.body;
     let result = await userDao.login(p);
-    console.log(result)
-    res.send({});
+    console.log(result);
+    if (false) {
+        // 此处需判断登录失败
+        res.send({ err: '' });
+    } else {
+        req.session.userId = result.id;
+        req.session.user = result;
+        res.send({ user: result });
+    }
 });
 
 router.post('/create', async function (req, res, next) {
     let p = req.body;
     let result = await userDao.create(p);
-    console.log(result)
+    console.log(result);
     res.send({});
 });
 
