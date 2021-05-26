656ffb26d6ccf6e3454ad9dc68aaa822f916d09c
zhangw <786014381@qq.com>
2021-05-26 17:33:30 +0800

    解决跨域

--- question-server/app.js
+++ question-server/app.js
@@ -67,7 +67,7 @@ MongoClient.connect(
 );
 
 app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
